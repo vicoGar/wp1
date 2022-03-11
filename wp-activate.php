42d6802e6fabde9a27e7c9c0ce2f450d92299951
Victor Garcia <vico99@gmail.com>
2022-03-10 18:24:40 -0600

    Update wp-activate.php

--- wp-activate.php
+++ wp-activate.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Confirms that the activation key that is sent in an email after a user signs
- * up for a new site matches the key for that user and then displays confirmation.
+ * up for a new site matches the key for theeeeeeat user and then displays confirmation.
  *
  * @package WordPress
  */
